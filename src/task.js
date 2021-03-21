92a25f27b4a151f70e20ff52575807662072dbac
pbsmith82 <pbsmith82@gmail.com>
2021-03-21 11:08:15 -0400

    Done.

---
+++ src/task.js
@@ -0,0 +1,15 @@
+class Task {
+    constructor(description) {
+      this.description = description;
+    }
+  
+    render() {
+      return `
+        <li>
+          ${this.description}
+          <button data-description="${this.description}">X</button>
+        </li>
+        `;
+    }
+  }
+  
