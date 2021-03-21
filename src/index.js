a2201575d18c385e5b7b82973ac935461e7ff116
pbsmith82 <pbsmith82@gmail.com>
2021-03-20 22:10:07 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let counter = 0
+  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    const task = document.getElementById("new-task-description");
+    const tasklist = document.getElementById("list")
+    counter += 1
+    tasklist.innerHTML += `<li> ${task.value} <button id="delete${counter}">x</button></li>`
+      tasklist.addEventListener("click", function(event) {
+    event.toElement.parentElement.outerHTML = ""
+    counter -= 1
+    
+  })
+    
+    event.preventDefault();
+
+}, false);
+
 });
